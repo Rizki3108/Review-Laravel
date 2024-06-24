c512d51545826004fbc293c3c40e0bf88fcb8a5a
Rizki3108 <muhammadalirizki583@gmail.com>
2024-06-24 16:02:35 +0700

    frontend

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.backend');
+    return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(
+    
+);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
